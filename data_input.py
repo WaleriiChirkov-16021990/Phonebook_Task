41555f48f043e8dec1cfe7e768183e883730aa4f
errasti <errasti007@yandex.ru>
2022-10-15 18:37:49 +0300

    record write possibility added and tested error removed

--- data_input.py
+++ data_input.py
@@ -5,5 +5,5 @@ def create_record(surname, name, phone_number, phone_info):
     new_record = f'{surname.title()}, {name.title()}, {phone_number}, {phone_info.title()}'
     records_data.append(new_record)
 
-    with open('records_db.txt', 'a', newline="\n", encoding='utf-8') as file:
+    with open('records_db.txt', 'a', encoding='utf-8') as file:
         file.write(new_record + '\n')
